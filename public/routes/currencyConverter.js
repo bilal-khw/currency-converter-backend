77368c79ab4f9076d877d4eac43520d1fbfecc9f
mike9090 <bilal@jumppace.com>
2024-10-21 23:41:16 +0500

    gitignore change

--- public/routes/currencyConverter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const currencyConverter_1 = require("../controllers/currencyConverter");
-let currencyRouter = (0, express_1.Router)();
-currencyRouter.get("/convert", currencyConverter_1.convertCurrencyController);
-currencyRouter.get("/list-currency", currencyConverter_1.fetchCurrencyListController);
-exports.default = currencyRouter;
